5e1f9f5fb72c26ede0d423a7992508bf0087d2ff
Nicolò Valenza <nico.vale.spazio@outlook.com>
2020-07-18 01:06:27 +0200

    add my proxy https://proxy-1blogapp.herokuapp.com/

--- public/js/parse_rss.js
+++ public/js/parse_rss.js
@@ -1,7 +1,7 @@
 //questo codiceesgue il parsing del feed rss ddi 1bcremona blog per cedere i nuovi artcoli
 //this code parse the rrs feed or 1bcremonablog.com to control new article
 
-const proxyurl = "https://cors-anywhere.herokuapp.com/";
+const proxyurl = "https://proxy-1blogapp.herokuapp.com/";
 const rss_url = "https://1bcremonablog.com/feed/";
 fetch(proxyurl+rss_url)
   .then(response => response.text())
@@ -13,14 +13,14 @@ fetch(proxyurl+rss_url)
     items.forEach(el => {
       var linkart = el.querySelector("link").innerHTML;
       var titleart = el.querySelector("title").innerHTM;
-      html += `
+      html += `<div class="articlecontainer">
         <div class="article">
           <h1>
           ${el.querySelector("title").innerHTML}<br>
           <a href="/articolo">  <button id="openlink" onclick="sessionStorage.setItem('titlearticle1bcremonablog', '${titleart}');sessionStorage.setItem('linkarticle1bcremonablog', '${linkart}')">Leggi l'articolo</button></a>
             </h1>
-                    </div>`;
+                    </div></div>`;
         });
 
-    document.body.insertAdjacentHTML("beforeend", html);
+    document.getElementById("root").innerHTML += html;
     });
